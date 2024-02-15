2463fab46f5f04b371a98adf950a2ed991a267b5
Ahmad Zeeshan <zesshan92322@gmail.com>
2024-02-15 12:30:19 +0500

    Adding Animations and different levels
    Almost complete game(adding levels and main menu and restart panel) and all animations are done!!

--- Through Scratch/Assets/Scripts/PlayerCollider.cs
+++ Through Scratch/Assets/Scripts/PlayerCollider.cs
@@ -4,7 +4,9 @@ using UnityEngine.SceneManagement;
 public class PlayerCollider : MonoBehaviour
 {
     public Transform player;
-    private float delayTime = 0.5f;
+    private float delayTime = 1.5f;
+    private float animationdelayTime = 0f;
+    public GameObject CompleteLevelUI;
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Obstacles"))
@@ -18,10 +20,14 @@ public class PlayerCollider : MonoBehaviour
     {
         if (other.gameObject.CompareTag("CompleteLevel"))
         {
+            Invoke("CompleteLevel", animationdelayTime);
             Invoke("NextLevel", delayTime);
         }
     }
-
+    void CompleteLevel()
+    {
+        CompleteLevelUI.SetActive(true);
+    }
     private void FixedUpdate()
     {
         if (player.position.y < -1)
